927632af9fba1d7d5aa3c0fe346481ff057cb131
Hilman F. Rakhmad <hifraofficial@gmail.com>
2021-08-07 07:43:04 +0700

    Moved error handling to onPreResponse lifecycle

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const Hapi = require('@hapi/hapi');
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const songsService = new SongsService();
@@ -24,6 +25,37 @@ const init = async () => {
       service: songsService,
       validator: SongsValidator,
     },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+
+    // Jika ClientError
+    if (response instanceof ClientError) {
+      // membuat response baru dari response toolkit sesuai kebutuhan error handling
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    // Jika Server Error
+    if (response instanceof Error) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: 'Internal Server Error',
+      });
+      newResponse.code(500);
+      console.error(response);
+      return newResponse;
+    }
+
+    // jika bukan ClientError, lanjutkan dengan response sebelumnya (tanpa intervensi)
+    return response.continue || response;
   });
 
   await server.start();
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable camelcase */
-const ClientError = require('../exceptions/ClientError');
 
 const mapDBToModel = ({
   id,
@@ -21,24 +20,4 @@ const mapDBToModel = ({
   updatedAt: updated_at,
 });
 
-const sendErrorResponse = (error, h) => {
-  if (error instanceof ClientError) {
-    const response = h.response({
-      status: 'fail',
-      message: error.message,
-    });
-    response.code(error.statusCode);
-    return response;
-  }
-
-  // server error
-  const response = h.response({
-    status: 'error',
-    message: 'Server error',
-  });
-  response.code(500);
-  console.error(error);
-  return response;
-};
-
-module.exports = { mapDBToModel, sendErrorResponse };
+module.exports = { mapDBToModel };
